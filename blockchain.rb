1f9ae3d59b3001e5b1de2f5ed4205109655793f5
Wilmer Zwietering <wzwietering@gmail.com>
2017-12-04 21:55:22 +0100

    Blockchain in Ruby

---
+++ blockchain.rb
@@ -0,0 +1,130 @@
+require 'digest'
+require 'json'
+require 'net/http'
+require 'set'
+require 'uri'
+
+class Blockchain
+  def initialize
+    @chain = []
+    @current_transactions = []
+    @nodes = Set.new
+
+    self.new_block(100, 1)
+  end
+
+  def register_node(address)
+    uri = URI.parse(address)
+    @nodes.add(uri.host)
+  end
+
+  def valid_chain?(chain)
+    last_block = chain[0]
+    current_index = 1
+
+    while current_index < chain.length
+      block = chain[current_index]
+      puts "#{last_block}"
+      puts "#{block}"
+      puts "\n-----------\n"
+
+      if block[:previous_hash] != self.class.hash(last_block)
+        return false
+      end
+      if not self.class.valid_proof?(last_block[:proof], block[:proof])
+        return false
+      end
+
+      last_block = block
+      current_index += 1
+    end
+
+    return true
+  end
+
+  def resolve_conflicts?
+    neighbours = @nodes
+    new_chain = nil
+
+    # Only look for long chains
+    max_length = @chain.length
+
+    # Grab and verify the chains from all the nodes in our network
+    for node in neighbours
+      uri = URI.parse("http://#{node}/chain")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+
+      if response.code == 200
+        json = JSON.parse(response.body)
+        length = json["length"]
+        chain = json["chain"]
+
+        # Check if the length is longer and the chain is valid
+        if length > max_length and valid_chain?(chain)
+          max_length = length
+          new_chain = chain
+        end
+      end
+    end
+
+    # Replace our chain if we discovered a new, valid chain longer than ours
+    if new_chain
+      @chain = new_chain
+      return true
+    end
+
+    return false
+  end
+
+  def new_block(proof, previous_hash=nil)
+    block = {
+      :index => @chain.length + 1,
+      :timestamp => Time.now.utc,
+      :transactions => @current_transactions,
+      :proof => proof,
+      :previous_hash => (previous_hash or self.class.hash(@chain[-1])),
+    }
+
+    # Reset the current list of transactions
+    @current_transactions = []
+
+    @chain.push(block)
+    return block
+  end
+
+  def new_transaction(sender, recipient, amount)
+    @current_transactions.push({
+      :index => sender,
+      :recipient => recipient,
+      :amount => amount,
+    })
+
+    return last_block[:index] + 1
+  end
+
+  def self.hash(block)
+    block_string = JSON.dump(block).encode('utf-8')
+    return Digest::SHA256.hexdigest(block_string)
+  end
+
+  def proof_of_work(last_proof)
+    proof = 0
+    while self.class.valid_proof?(last_proof, proof) == false
+      proof += 1
+    end
+
+    return proof
+  end
+
+  def self.valid_proof?(last_proof, proof)
+    guess = "#{last_proof}#{proof}".encode('utf-8')
+    guess_hash = Digest::SHA256.hexdigest(guess)
+    return guess_hash[0...4] == "0000"
+  end
+
+  def last_block
+    return @chain[-1]
+  end
+end
